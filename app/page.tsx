2548e832d17f32c2b9885357f146768313ef13e7
Fernando Nagase <nagase.fernando@gmail.com>
2023-06-07 19:56:13 -0300

    move attribution to its own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
+import Attribution from '@/components/Attribution'
+
 export default function Home() {
     return (
-        <div>
-            Challenge by{' '}
-            <a href="https://www.frontendmentor.io?ref=challenge">
-                Frontend Mentor
-            </a>
-            . Coded by{' '}
-            <a href="https://github.com/fernandonagase">Fernando Nagase</a>.
-        </div>
+        <footer>
+            <Attribution />
+        </footer>
     )
 }
---
+++ components/Attribution/index.tsx
@@ -0,0 +1,12 @@
+export default function Attribution() {
+    return (
+        <div>
+            Challenge by{' '}
+            <a href="https://www.frontendmentor.io?ref=challenge">
+                Frontend Mentor
+            </a>
+            . Coded by{' '}
+            <a href="https://github.com/fernandonagase">Fernando Nagase</a>.
+        </div>
+    )
+}
