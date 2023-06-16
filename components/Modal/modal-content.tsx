0d19cf6f7dab46a80e8bf6b08692709450013e3e
Fernando Nagase <nagase.fernando@gmail.com>
2023-06-15 21:21:13 -0300

    trap tab focus inside modal

--- components/Modal/modal-content.tsx
+++ components/Modal/modal-content.tsx
@@ -1,4 +1,11 @@
-import { KeyboardEvent, ReactNode } from 'react'
+import React from 'react'
+import { ReactNode, useEffect, useRef } from 'react'
+
+// Seletor retirado do artigo de Hidde de Vries
+// Não é uma solução universal, mas, por hora, satisfaz este caso de uso
+// https://hidde.blog/using-javascript-to-trap-focus-in-an-element/
+const interactiveSelector =
+    'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])'
 
 type ModalContentProps = {
     children: ReactNode
@@ -6,7 +13,39 @@ type ModalContentProps = {
 }
 
 export default function ModalContent({ children, onClose }: ModalContentProps) {
-    function handleEsc(event: KeyboardEvent) {
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (!containerRef.current) throw Error('containerRef is not assigned')
+
+        const container = containerRef.current
+        const focusableEls =
+            container.querySelectorAll<HTMLElement>(interactiveSelector)
+        const firstFocusableEl = focusableEls[0]
+        const lastFocusableEl = focusableEls[focusableEls.length - 1]
+
+        function handleTab(event: KeyboardEvent) {
+            const pressedTab = event.key === 'Tab'
+            if (!pressedTab) return
+
+            if (event.shiftKey) {
+                if (document.activeElement === firstFocusableEl) {
+                    lastFocusableEl.focus()
+                    event.preventDefault()
+                }
+            } else {
+                if (document.activeElement === lastFocusableEl) {
+                    firstFocusableEl.focus()
+                    event.preventDefault()
+                }
+            }
+        }
+
+        container.addEventListener('keydown', handleTab)
+        return () => container.removeEventListener('keydown', handleTab)
+    }, [children])
+
+    function handleEsc(event: React.KeyboardEvent) {
         const pressedEsc = event.key === 'Escape'
         if (pressedEsc) onClose()
     }
@@ -19,6 +58,7 @@ export default function ModalContent({ children, onClose }: ModalContentProps) {
             aria-describedby="alertdialog-body"
             onKeyDown={handleEsc}
             tabIndex={-1}
+            ref={containerRef}
         >
             {children}
         </div>
