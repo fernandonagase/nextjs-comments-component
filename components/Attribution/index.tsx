4351efb566a6a4f3952aa5b0c75866506ffea003
Fernando Nagase <nagase.fernando@gmail.com>
2023-06-20 18:43:43 -0300

    style attribution component

--- components/Attribution/index.tsx
+++ components/Attribution/index.tsx
@@ -1,12 +1,19 @@
+import styles from './styles/attribution.module.scss'
+
 export default function Attribution() {
     return (
-        <div>
-            Challenge by{' '}
-            <a href="https://www.frontendmentor.io?ref=challenge">
-                Frontend Mentor
-            </a>
-            . Coded by{' '}
-            <a href="https://github.com/fernandonagase">Fernando Nagase</a>.
+        <div className={styles.attribution}>
+            <p>
+                Challenge by{' '}
+                <a href="https://www.frontendmentor.io?ref=challenge">
+                    Frontend Mentor
+                </a>
+                .
+            </p>{' '}
+            <p>
+                Coded by{' '}
+                <a href="https://github.com/fernandonagase">Fernando Nagase</a>.
+            </p>
         </div>
     )
 }
